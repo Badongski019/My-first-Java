640d971d8c0c5a7fa3b1de098c57a658a9572d55
Badongski019 <yamerouu23@gmail.com>
2025-08-20 06:29:17 +0800

    Add files via upload

---
+++ Hellowworld.java
@@ -0,0 +1,16 @@
+public class Hellowworld {
+  public static void main(String[] args) {
+	// Student Data
+	String studentName = "Clark Daren J. Villabroza";
+	int studentID = 30;
+	int studentAge = 18;
+	float studentFee = 77.25f;
+	char studentGrade = 'B';
+	// Print Variables
+	System.out.println("Student name:" + studentName);
+	System.out.println("Student id:" + studentID);
+	System.out.println("Student age:" + studentAge);
+	System.out.println("Student fee:" + studentFee);
+	System.out.println("Student grade:" + studentGrade);
+  }
+}
